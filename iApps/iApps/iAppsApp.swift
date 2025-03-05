43f54b75f8ed673b450da3edbfb1f21d80bd8cae
Przemyslaw Szlek <przemyslaw.szlek@neofonie.de>
2025-03-05 12:34:28 +0100

    new project

---
+++ iApps/iApps/iAppsApp.swift
@@ -0,0 +1,18 @@
+//  
+//  iAppsApp.swift
+//  iApps
+//
+//  Created by Przemysław Szlęk on 05/03/2025.
+//  Copyright © 2025 Przemysław Szlęk. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
